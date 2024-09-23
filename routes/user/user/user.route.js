81d14dafe485379c7618369aae08441850865163
kevalsatani <kevalsatanisk9899@gmail.com>
2024-09-23 19:34:21 +0530

    Revert "add auth middlware in user routes."
    This reverts commit 60c3920a373ab52f01d177697b0d7a701dfdf9c5.

--- routes/user/user/user.route.js
+++ routes/user/user/user.route.js
@@ -2,8 +2,6 @@ import express from 'express';
 import { userController } from 'controllers/user';
 import { userValidation } from 'validations/user';
 import validate from 'middlewares/validate';
-import auth from "middlewares/auth";
-
 
 const router = express.Router();
 router
@@ -11,26 +9,26 @@ router
   /**
    * get list of friends
    * */
-  .get(auth(),validate(userValidation.fetchAllFriends), userController.fetchAllFriends);
+  .get(validate(userValidation.fetchAllFriends), userController.fetchAllFriends);
 
 router
   .route('/friend')
   /**
    * send friend request
    */
-  .post(auth(),validate(userValidation.sendFriendRequest), userController.sendFriendRequest);
+  .post(validate(userValidation.sendFriendRequest), userController.sendFriendRequest);
 router
   .route('/friends-request')
   /**
    * Fetch all friend Request with pagination
    * */
-  .get(auth(),validate(userValidation.paginatedUser), userController.getFriendRequestWithPagination);
+  .get(validate(userValidation.paginatedUser), userController.getFriendRequestWithPagination);
 
 router
   .route('/friends-request/:id')
   /**
    * id - Response to Friend Requests
    */
-  .put(auth(),validate(userValidation.friendRequest), userController.friendRequest);
+  .put(validate(userValidation.friendRequest), userController.friendRequest);
 
 export default router;
